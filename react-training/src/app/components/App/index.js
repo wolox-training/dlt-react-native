33ec84868afbb6f191e4756bde55ffc3c3fd6500
Daniela Lotero Tapias <danielaloterotapias@Woloxs-MacBook-Pro.local>
2018-08-14 11:58:17 -0500

    merging

--- react-training/src/app/components/App/index.js
+++ react-training/src/app/components/App/index.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-=======
-import React from 'react';
->>>>>>> feature/login
 import { Provider } from 'react-redux';
 
 import Login from '../../screens/Login';
 import Game from '../../screens/Game';
-<<<<<<< HEAD
 import { createAppStore } from '../../../redux/store';
 
 import style from './styles.scss';
@@ -30,20 +25,5 @@ const App = () => {
     </div>
   );
 };
-=======
-import store from '../../../redux/store';
-
-import style from './styles.scss';
-
-const App = () => (
-  <div className={style.app}>
-    <main className={style.appMain}>
-      <Provider store={store}>
-        <Game store={store} />
-      </Provider>
-    </main>
-  </div>
-);
->>>>>>> feature/login
 
 export default App;
