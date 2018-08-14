6025a51f838140cda14dec53c4a6f7455de955d5
Daniela Lotero Tapias <danielaloterotapias@Woloxs-MacBook-Pro.local>
2018-08-14 12:00:19 -0500

    fixing merge problems

--- react-training/src/app/components/App/index.js
+++ react-training/src/app/components/App/index.js
@@ -4,26 +4,23 @@ import { Provider } from 'react-redux';
 
 import Login from '../../screens/Login';
 import Game from '../../screens/Game';
-import { createAppStore } from '../../../redux/store';
+import store from '../../../redux/store';
 
 import style from './styles.scss';
 
-const App = () => {
-  const store = createAppStore();
-  return (
-    <div className={style.app}>
-      <main className={style.appMain}>
-        <Provider store={store}>
-          <Router>
-            <Fragment>
-              <Route exact path="/" component={Login} />
-              <Route path="/game" component={Game} />
-            </Fragment>
-          </Router>
-        </Provider>
-      </main>
-    </div>
-  );
-};
+const App = () => (
+  <div className={style.app}>
+    <main className={style.appMain}>
+      <Provider store={store}>
+        <Router>
+          <Fragment>
+            <Route exact path="/" component={Login} />
+            <Route path="/game" component={Game} />
+          </Fragment>
+        </Router>
+      </Provider>
+    </main>
+  </div>
+);
 
 export default App;
