26efb1cb9ccd92cdd9a8052050c35a86b74dafe7
Daniela Lotero Tapias <danielaloterotapias@Woloxs-MacBook-Pro.local>
2018-08-14 08:30:45 -0500

    addressing comments

--- react-training/src/redux/game/reducer.js
+++ react-training/src/redux/game/reducer.js
@@ -28,12 +28,10 @@ export default function reducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.JUMP_TO:
       const { step } = action; 
-      return Object.assign({}, ...state,
-        {
-          stepNumber: step,
-          xIsNext: step % 2 === 0
-        }
-      );
+      return {
+        ...state, stepNumber: step,
+        xIsNext: step % 2 === 0
+      };
     case actionTypes.PLAYER_MOVE:
       const { squareNum } = action;
 
@@ -42,16 +40,15 @@ export default function reducer(state = initialState, action) {
       const squares = current.squares.slice();
       const winner = calculateWinner(squares);
       if (winner || squares[squareNum])
-        return Object.assign({}, ...state, { winner });
+        return { ...state, winner: winner };
 
       squares[squareNum] = state.xIsNext ? 'X' : 'O';
-      return Object.assign({}, 
+      return {
         ...state,
-        {
-          history: history.concat([{ squares }]),
-          stepNumber: history.length,
-          xIsNext: !state.xIsNext
-        });
+        history: history.concat([{ squares }]),
+        stepNumber: history.length,
+        xIsNext: !state.xIsNext
+      }
     default:
       return state;
   }
