e153433417295fc19c4b1614b5e01cac9ee93718
Daniela Lotero Tapias <danielaloterotapias@Woloxs-MacBook-Pro.local>
2018-08-23 10:03:41 -0500

    adding completeState

--- react-training/src/redux/auth/reducer.js
+++ react-training/src/redux/auth/reducer.js
@@ -1,9 +1,9 @@
-import { createReducer, onLoading, onSuccess, onFailure, onReadValue } from 'redux-recompose';
+import { createReducer, completeState, onLoading, onSuccess, onFailure, onReadValue } from 'redux-recompose';
 import Immutable from 'seamless-immutable';
 
 import { actionTypes } from '../auth/action';
 
-const initialState = Immutable({ auth: null });
+const initialState = completeState({ auth: null });
 
 const reducerDescriptor = {
   [actionTypes.LOGIN_REQUEST]: onLoading(),
@@ -13,4 +13,4 @@ const reducerDescriptor = {
   [actionTypes.LOG_OUT]: onReadValue()
 };
 
-export default createReducer(initialState, reducerDescriptor);
+export default createReducer(Immutable(initialState), reducerDescriptor);
