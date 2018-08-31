dd85d52b9e17886bf769c37f3d4a9ebd95c1494a
Daniela Lotero Tapias <danielaloterotapias@Woloxs-MacBook-Pro.local>
2018-08-31 11:27:33 -0500

    adding route constant

--- react-native/trainingTodoList/src/app/screens/Books/index.js
+++ react-native/trainingTodoList/src/app/screens/Books/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import { BookDetail } from '../../../constants/routes';
+
 import BookList from './components/BookList';
 
 class Books extends Component {
   navigateToDetail = (id, title) => {
     const { navigation } = this.props;
-    navigation.navigate('bookDetail', { id, title });
+    navigation.navigate(BookDetail, { id, title });
   };
 
   render() {
